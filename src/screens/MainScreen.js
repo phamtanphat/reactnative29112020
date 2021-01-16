13afd7c6c4c26c525251113ac3336488173aee9e
phamtanphat <phatdroid94@gmail.com>
2021-01-16 20:28:33 +0700

    bai tap so 1

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -5,14 +5,83 @@ import {Text, View} from 'react-native';
 export default class MainScreen extends Component {
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: 'antiquewhite',
-          flexDirection: 'column'
-        }}>
-        <Text> Text 1 </Text>
-        <Text> Text 2 </Text>
+      <View style={{flex: 1}}>
+        <View style={{flexDirection: 'column', flex: 1}}>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: 'orange',
+            }}>
+            <Text style={{color: 'black'}}>A</Text>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: 'blue',
+            }}>
+            <Text style={{color: 'black'}}>B</Text>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: 'pink',
+            }}>
+            <Text style={{color: 'black'}}>C</Text>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: 'gray',
+            }}>
+            <Text style={{color: 'black'}}>D</Text>
+          </View>
+        </View>
+        <View style={{flexDirection: 'row', flex: 1}}>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: 'orange',
+            }}>
+            <Text style={{color: 'black'}}>A</Text>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: 'blue',
+            }}>
+            <Text style={{color: 'black'}}>B</Text>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: 'pink',
+            }}>
+            <Text style={{color: 'black'}}>C</Text>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: 'gray',
+            }}>
+            <Text style={{color: 'black'}}>D</Text>
+          </View>
+        </View>
       </View>
     );
   }
