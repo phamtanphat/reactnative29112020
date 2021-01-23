687dff3592ae5f0f31c2dbba6382f7d2f30afb76
phamtanphat <phatdroid94@gmail.com>
2021-01-23 20:22:27 +0700

    update layout indicator android

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -72,6 +72,7 @@ export default class MainScreen extends Component {
                 });
               }}
               tintColor="red"
+              colors={['red', 'blue', 'green']}
             />
           }
         />
