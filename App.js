e7b619ab1dddda9f7c450846ec942fa6f6ea8a77
phamtanphat <phatdroid94@gmail.com>
2021-02-02 21:06:31 +0700

    reducer

--- App.js
+++ App.js
@@ -5,49 +5,31 @@ import MainScreen from './src/screens/MainScreen';
 import Box from './src/components/Box';
 import Form from './src/components/Form';
 import Filter from './src/components/Filter';
-import {createStore} from 'redux';
+import {createStore, combineReducers} from 'redux';
 import {Provider} from 'react-redux';
 
-const defaultStore = {
-  words: [
-    {id: 1, en: 'One', vn: 'Một', isMemorized: false},
-    {id: 2, en: 'Two', vn: 'Hai', isMemorized: true},
-    {id: 3, en: 'Three', vn: 'Ba', isMemorized: true},
-    {id: 4, en: 'Four', vn: 'Bốn', isMemorized: false},
-    {id: 5, en: 'Five', vn: 'Năm', isMemorized: true},
-  ].reverse(),
-  filterMode: 'Show_All',
-};
+const defaultWord = [
+  {id: 1, en: 'One', vn: 'Một', isMemorized: false},
+  {id: 2, en: 'Two', vn: 'Hai', isMemorized: true},
+  {id: 3, en: 'Three', vn: 'Ba', isMemorized: true},
+  {id: 4, en: 'Four', vn: 'Bốn', isMemorized: false},
+  {id: 5, en: 'Five', vn: 'Năm', isMemorized: true},
+].reverse();
 
-const store = createStore((state = defaultStore, action) => {
-  if (action.type === 'TOGGLE_WORD') {
-    const newWords = state.words.map((word) => {
-      if (word.id === action.item.id) {
-        return {...word, isMemorized: !action.item.isMemorized};
-      }
-      return word;
-    });
-    return {...state, words: newWords};
-  }
-  if (action.type === 'REMOVE_WORD') {
-    const newWords = state.words.filter((word) => {
-      if (word.id === action.item.id) {
-        return false;
-      }
-      return true;
-    });
-    return {...state, words: newWords};
-  }
-  if (action.type === 'ADD_WORD') {
-    const newWords = Object.assign([], state.words);
-    newWords.unshift(action.word);
-    return {...state, words: newWords};
-  }
-  if (action.type === 'SET_FILTER_MODE') {
-    return {...state, filterMode: action.filterMode};
-  }
+function wordReducer(state = defaultWord, action) {
   return state;
+}
+
+function filterModeReducer(state = 'Show_All', action) {
+  return state;
+}
+
+const rootReducer = combineReducers({
+  words: wordReducer,
+  filterMode: filterModeReducer,
 });
+
+const store = createStore(rootReducer);
 
 export default class App extends Component {
   render() {
@@ -60,3 +42,31 @@ export default class App extends Component {
     );
   }
 }
+
+// if (action.type === 'TOGGLE_WORD') {
+//   const newWords = state.words.map((word) => {
+//     if (word.id === action.item.id) {
+//       return {...word, isMemorized: !action.item.isMemorized};
+//     }
+//     return word;
+//   });
+//   return {...state, words: newWords};
+// }
+// if (action.type === 'REMOVE_WORD') {
+//   const newWords = state.words.filter((word) => {
+//     if (word.id === action.item.id) {
+//       return false;
+//     }
+//     return true;
+//   });
+//   return {...state, words: newWords};
+// }
+// if (action.type === 'ADD_WORD') {
+//   const newWords = Object.assign([], state.words);
+//   newWords.unshift(action.word);
+//   return {...state, words: newWords};
+// }
+// if (action.type === 'SET_FILTER_MODE') {
+//   return {...state, filterMode: action.filterMode};
+// }
+// return state;
