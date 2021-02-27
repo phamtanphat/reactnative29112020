e640282f3872fa53371eb752b555562fd3653a91
phamtanphat <phatdroid94@gmail.com>
2021-02-27 21:02:19 +0700

    axios

--- App.js
+++ App.js
@@ -2,13 +2,27 @@
 import React, {Component} from 'react';
 import {Text, View, SafeAreaView} from 'react-native';
 import MainScreen from './src/screens/MainScreen';
-import Box from './src/components/Box';
-import Form from './src/components/Form';
-import Filter from './src/components/Filter';
 import {Provider} from 'react-redux';
 import store from './src/redux/store';
+import axios from 'axios';
 
 export default class App extends Component {
+  componentDidMount() {
+    const url = 'https://servernode29122020.herokuapp.com/word';
+    const params = new URLSearchParams();
+    params.append('en', 'Four');
+    params.append('vn', 'Bốn');
+
+    axios
+      .post(url, params, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      })
+      .then((response) => console.log(response.data))
+      .catch((error) => console.log(error));
+  }
+
   render() {
     return (
       <SafeAreaView style={{flex: 1}}>
